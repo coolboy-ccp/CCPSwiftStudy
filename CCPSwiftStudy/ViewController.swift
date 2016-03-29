9b760426fc6e320a8208276720a67b3f24d13eb4
brian-chu <chuchengpeng@liqunfeidemac-mini.local>
2016-03-29 16:27:53 +0800

    Initial Commit

---
+++ CCPSwiftStudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CCPSwiftStudy
+//
+//  Created by liqunfei on 16/3/29.
+//  Copyright © 2016年 chuchengpeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
