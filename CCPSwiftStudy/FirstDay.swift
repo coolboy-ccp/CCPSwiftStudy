df240c3bb30e0a1420312278dc3ec11429882aae
brian-chu <chuchengpeng@liqunfeideMac-mini.local>
2016-03-29 16:35:35 +0800

    从零开始学习swift
    first day

---
+++ CCPSwiftStudy/FirstDay.swift
@@ -0,0 +1,14 @@
+//
+//  FirstDay.swift
+//  CCPSwiftStudy
+//
+//  Created by liqunfei on 16/3/29.
+//  Copyright © 2016年 chuchengpeng. All rights reserved.
+//
+
+import UIKit
+
+class FirstDay: NSObject {
+   let minNumber = 10
+   var currentNumber = 11
+}
